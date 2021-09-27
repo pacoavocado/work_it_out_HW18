d01c553bfe7c4527df33f4bd982035441f36e9a2
Micah Conn <mcoxconn@gmail.com>
2021-09-26 23:22:39 -0500

    I think ive found all the routes i need to figure out how to data them

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -11,9 +11,6 @@ router.get("/stats", (req, res) => {
   return;
 });
 
-
-
-
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
@@ -24,21 +21,34 @@ router.get("/api/workouts", (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-  });
-  
-  router.post("/api/workouts", async ({ body }, res) => {
-    try {
-      const response = await Workout.create({body})
-      res.json(response);
-    }
-    catch{(err => {
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
-  }});
-  
-  router.put("/api/workouts/:id", async ({ body }, res) => {
-    const response = await Workout.find(body)
+});
+
+router.post("/api/workouts", async ({ body }, res) => {
+  try {
+    const response = await Workout.create({ body });
+    res.json(response);
+  } catch {
+    (err) => {
+      console.log(err);
+      res.status(400).json(err);
+    };
+  }
+});
+
+router.put("/api/workouts/:id", async ({ body }, res) => {
+  const response = await Workout.find(body)
     .then((response) => {
       res.json(response);
     })
@@ -46,7 +56,6 @@ router.get("/api/workouts", (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-  });
-
+});
 
 module.exports = router;
